5672c85cb395a502793f5c538a3f0defd9def7cd
shafeekrahman04 <shafeekrahman04@gmail.com>
2025-09-04 11:06:48 +0530

    mvc template inserted

---
+++ src/main/java/com/bus/booking/management/BusBookingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bus.booking.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBookingManagementApplication.class, args);
+	}
+
+}
